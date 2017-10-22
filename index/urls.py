c856e60e67e267eaa204bf477f4753cbdd90feae
Michael Henry <michael.henry@sure.technology>
2017-10-22 17:38:21 -0400

    Removing pyc and other unnecessary files from repo

--- index/urls.py
+++ index/urls.py
@@ -19,5 +19,8 @@ from index import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^new/', views.new, name='index'),
+
+
 ]
--- index/views.py
+++ index/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render, HttpResponse
 
 
 def index(request):
-    return render(request, 'index/home.html',)
+    return render(request, 'index/home.html')
+
+
+def new(request):
+    return render(request, 'index/new.html')
+
